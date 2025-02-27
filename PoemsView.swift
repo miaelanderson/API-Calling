02a9f04ae4f057f39ef0116ee3fd00dd887fb1b1
Mia <miaelanderson@gmail.com>
2025-02-27 10:02:22 -0600

    set up poems view

---
+++ PoemsView.swift
@@ -0,0 +1,68 @@
+//
+//  PoemsView.swift
+//  API Calling
+//
+//  Created by Mia Anderson on 2/27/25.
+//
+
+import SwiftUI
+
+struct PoemsView: View {
+    @State private var poems = [Poem]()
+    @State private var showingAlert = false
+    let author: String
+    var body: some View {
+        List(poems) { poem in
+            NavigationLink {
+                ScrollView {
+                    VStack {
+                        Text(poem.title)
+                        Text("by \(author)").padding(.bottom)
+                        ForEach(poem.lines, id: \.self) { line in
+                            Text(line)
+                                .font(.subheadline)
+                                .foregroundColor(.gray)
+                        }
+                        Spacer()
+                    }
+                }
+            } label: {
+                Text(poem.title)
+            }
+        }
+        .navigationTitle("Poems by " + author)
+        .navigationBarTitleDisplayMode(.inline)
+        .task {
+            await getPoems()
+        }
+        .alert(isPresented: $showingAlert) {
+            Alert(title: Text("Loading Error"),
+                  message: Text("There was a problem loading the poetry authors"),
+                  dismissButton: .default(Text("OK")))
+        }
+    }
+    func getPoems() async {
+        let query = "https://poetrydb.org/author/" + author + "/title,lines"
+        if let url = URL(string: query) {
+            if let (data, _) = try? await URLSession.shared.data(from: url) {
+                if let decodedResponse = try? JSONDecoder().decode([Poem].self, from: data) {
+                    poems = decodedResponse
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    PoemsView(author: "Walt Whitman")
+}
+
+struct Poem: Identifiable, Codable {
+    let id = UUID()
+    let title: String
+    let lines: [String]
+    
+    enum CodingKeys: String, CodingKey {
+        case title, lines
+    }
+}
